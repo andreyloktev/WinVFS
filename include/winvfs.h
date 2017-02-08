2154d192eb8bf1d30e8d84c39160efb1ebd49b4d
Andrey Loktev <loktev.andrei@gmail.com>
2017-02-08 23:54:15 +0300

    Added winvfs file callbacks

--- include/winvfs.h
+++ include/winvfs.h
@@ -1,31 +1,40 @@
 #pragma once
 
 #include <ntifs.h>
+#include "winvfs/winvfs_file_operations.h"
+#include "winvfs/winvfs_volume_operations.h"
 
 typedef void *PWIN_VFS;
 
-typedef NTSTATUS (*WinVfsCallback)(  );
 
-/**
-* WinVFS callbacks which are called by WinVFS library.
-*/
-typedef struct _WIN_VFS_CALLBACKS
+
+
+//========= WinVFS volume operation callbacks which are called by WinVFS library. ====================
+
+
+typedef struct _WIN_VFS_VOLUME_OPERATIONS
 {
-    WinVfsCallback WinVfsDispatchLookup;
-    WinVfsCallback WinVfsDispatchCreate;
-    WinVfsCallback WinVfsDispatchCleanup;
-    WinVfsCallback WinVfsDispatchClose;
-    WinVfsCallback WinVfsDispatchFileRead;
-    WinVfsCallback WinVfsDispatchFileWrite;
-    WinVfsCallback WinVfsDispatchReadDirectory;
-} WIN_VFS_CALLBACKS, *PWIN_VFS_CALLBACKS;
+} WIN_VFS_VOLUME_OPERATIONS, *PWIN_VFS_VOLUME_OPERATIONS;
+
+//====================================================================================================
 
 /**
 * Create and initialize WIN_VFS object.
-* @in pFileSystemDevObjName device object name representing  the driver itself so that requests can be targeted to the driver. For example "mount" request.
 * @in pCallbacks callbacks which are called by WinVFS library
 */
-NTSTATUS InitializeWinVFS( _In_ PUNICODE_STRING pFileSystemDevObjName, _In_ PWIN_VFS_CALLBACKS pCallbacks, _Out_ PWIN_VFS *ppWinVFS );
+NTSTATUS InitializeWinVFS( 
+                            _In_   PWIN_VFS_FILE_OPERATIONS    pFileOperationss
+                            ,_In_   PWIN_VFS_VOLUME_OPERATIONS  pVolumeOperations
+                            ,_Out_  PWIN_VFS*                   ppWinVFS
+                        );
+
+/**
+* Creaete a device object representing  the driver itself so that requests can be targeted to the driver. For example "mount" request.
+* @in pFileSystemDevObjName device object name.
+* @in pWinVFS WinVFS object
+* @out status code
+*/
+NTSTATUS CreateWinVFSDeviceObject( _In_ PUNICODE_STRING pFileSystemDevObjName, _In_ PWIN_VFS pWinVFS );
 
 /**
 * Routine to initialize DRIVER_OBJECT structrue
